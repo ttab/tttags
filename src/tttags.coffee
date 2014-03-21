ttab/tttags

commit 0f361967d38b7d84c8194a154d36886d5d5377a1

design tweaks

src/tttags.coffee
@@ -80,7 +80,7 @@ class TTTags
         return this if @has tag
         return this unless @opts.allowNew or opts.force or (inList = @inList tag)
         $el = $ subst @opts.template, tag:tag
-		$el.addClass @opts.className if @opts.className
+        $el.addClass @opts.className if @opts.className
         if @$inputwrap
             @$inputwrap.before($el)
         else

src/tttags.less
@@ -1,10 +1,13 @@
 @import "bower_components/bootswatchtt/tt/variables.less";
 
 @liheight: 1.8em;
+@tagspacing: 0.3em;
+@inputcolor: @gray;
 
 .tttags {
     cursor: text;
     .tttplaceholder {
+        color: lighten(@inputcolor, 10%);
         display: none;
         font-style: italic;
         opacity: 0.6;
@@ -14,8 +17,9 @@
     }
     .tttinputwrap {
         position: relative;
+		margin-left: @tagspacing;
         .tttinput {
-            color: @gray-lighter;
+            color: @inputcolor;
             display: inline-block;
             min-width: 10px;
             &:after {
@@ -50,7 +54,7 @@
                     display: none;
                     line-height: @liheight;
                     height: @liheight;
-                    color: lighten(@gray-lighter, 10%);
+                    color: lighten(@inputcolor, 10%);
                     text-shadow: 0 1px 2px rgba(0,0,0,0.4);
                     padding: 0 0.5em;
                     cursor: pointer;
@@ -80,4 +84,7 @@
     &:after {
         content: '\00a0\00a0';
     }
+	.tag {
+		margin-left: @tagspacing;
+	}
 }

tttags.css
@@ -2,6 +2,7 @@
   cursor: text;
 }
 .tttags .tttplaceholder {
+  color: #6f6f6f;
   display: none;
   font-style: italic;
   opacity: 0.6;
@@ -11,9 +12,10 @@
 }
 .tttags .tttinputwrap {
   position: relative;
+  margin-left: 0.3em;
 }
 .tttags .tttinputwrap .tttinput {
-  color: #eeeeee;
+  color: #555555;
   display: inline-block;
   min-width: 10px;
 }
@@ -50,7 +52,7 @@
   display: none;
   line-height: 1.8em;
   height: 1.8em;
-  color: #ffffff;
+  color: #6f6f6f;
   text-shadow: 0 1px 2px rgba(0, 0, 0, 0.4);
   padding: 0 0.5em;
   cursor: pointer;
@@ -72,4 +74,7 @@
 }
 .tttags:after {
   content: '\00a0\00a0';
+}
+.tttags .tag {
+  margin-left: 0.3em;
 }

tttags.js
@@ -1,8 +1,7 @@
 // Generated by CoffeeScript 1.6.3
 (function() {
   var $, ACT, ON, SEL, TTTags, defaultOpts, subst,
-    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
-    _this = this;
+    __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
 
   $ = jQuery;
 
@@ -38,6 +37,22 @@
 
   TTTags = (function() {
     function TTTags(opts) {
+      this._onInputKeyUp = __bind(this._onInputKeyUp, this);
+      this._onInputKeyDown = __bind(this._onInputKeyDown, this);
+      this._onInputBlur = __bind(this._onInputBlur, this);
+      this._onInputFocus = __bind(this._onInputFocus, this);
+      this._onFocus = __bind(this._onFocus, this);
+      this._onClick = __bind(this._onClick, this);
+      this._onMouseDown = __bind(this._onMouseDown, this);
+      this._maybeShowPlaceholder = __bind(this._maybeShowPlaceholder, this);
+      this._doSelect = __bind(this._doSelect, this);
+      this._listStep = __bind(this._listStep, this);
+      this._suggest = __bind(this._suggest, this);
+      this._buildListAndInput = __bind(this._buildListAndInput, this);
+      this._trigger = __bind(this._trigger, this);
+      this.destroy = __bind(this.destroy, this);
+      this.isActive = __bind(this.isActive, this);
+      this.tags = __bind(this.tags, this);
       this.add = __bind(this.add, this);
       this.inList = __bind(this.inList, this);
       this.has = __bind(this.has, this);
@@ -134,24 +149,32 @@
       if (!(this.opts.allowNew || opts.force || (inList = this.inList(tag)))) {
         return this;
       }
-      return $el = $(subst(this.opts.template, {
+      $el = $(subst(this.opts.template, {
         tag: tag
       }));
+      if (this.opts.className) {
+        $el.addClass(this.opts.className);
+      }
+      if (this.$inputwrap) {
+        this.$inputwrap.before($el);
+      } else {
+        if ($('.tag', this.$el).last().after($el).length === 0) {
+          this.$el.prepend($el);
+        }
+      }
+      if (!opts.silent) {
+        this._trigger('add', 'onTagAdd', tag, $el, !inList);
+      }
+      return this;
     };
 
-    return TTTags;
-
-  })();
-
-  $el.addClass(this.opts.className(this.opts.className ? (this.$inputwrap ? this.$inputwrap.before($el) : $('.tag', this.$el).last().after($el).length === 0 ? this.$el.prepend($el) : void 0, !opts.silent ? this._trigger('add', 'onTagAdd', tag, $el, !inList) : void 0, this) : void 0));
-
-  ({
-    tags: function(toSet) {
+    TTTags.prototype.tags = function(toSet) {
+      var _this = this;
       if (typeof toSet === 'string') {
         toSet = [toSet];
       }
       if ($.isArray(toSet)) {
-        _this.tags().forEach(function(tag) {
+        this.tags().forEach(function(tag) {
           return _this.remove(tag, {
             silent: true
           });
@@ -162,46 +185,49 @@
             silent: true
           });
         });
-        _this._maybeShowPlaceholder();
-        return _this;
+        this._maybeShowPlaceholder();
+        return this;
       } else {
-        return ($('.tag', _this.$el).map(function() {
+        return ($('.tag', this.$el).map(function() {
           return $(this).text();
         })).toArray();
       }
-    },
-    isActive: function() {
-      return _this.$el.hasClass(ACT);
-    },
-    destroy: function() {
+    };
+
+    TTTags.prototype.isActive = function() {
+      return this.$el.hasClass(ACT);
+    };
+
+    TTTags.prototype.destroy = function() {
       var _ref, _ref1, _ref2;
-      if (_this.$input) {
-        _this.$input.off();
+      if (this.$input) {
+        this.$input.off();
       }
-      if (_this.$el) {
-        _this.$el.off();
+      if (this.$el) {
+        this.$el.off();
       }
-      if ((_ref = _this.$input) != null) {
+      if ((_ref = this.$input) != null) {
         _ref.remove();
       }
-      _this.$input = null;
-      if ((_ref1 = _this.$list) != null) {
+      this.$input = null;
+      if ((_ref1 = this.$list) != null) {
         _ref1.remove();
       }
-      _this.$list = null;
-      if ((_ref2 = _this.$inputwrap) != null) {
+      this.$list = null;
+      if ((_ref2 = this.$inputwrap) != null) {
         _ref2.remove();
       }
-      _this.$inputwrap = null;
-      _this.$el.data('tttags', null);
-      return _this.$el = null;
-    },
-    _trigger: function(event, callback, tag, el, isNew) {
+      this.$inputwrap = null;
+      this.$el.data('tttags', null);
+      return this.$el = null;
+    };
+
+    TTTags.prototype._trigger = function(event, callback, tag, el, isNew) {
       var cb, evt;
       evt = {
         type: event,
-        tags: _this.tags(),
-        src: _this
+        tags: this.tags(),
+        src: this
       };
       if (tag) {
         evt.tag = tag;
@@ -212,59 +238,64 @@
       if (typeof isNew === 'boolean') {
         evt.isNew = isNew;
       }
-      _this.trigger(event, evt);
-      cb = _this.opts[callback];
+      this.trigger(event, evt);
+      cb = this.opts[callback];
       if (typeof cb === 'function') {
         return cb(evt);
       }
-    },
-    _buildListAndInput: function() {
-      var $ol, s;
-      if (!_this.$input) {
-        _this.$inputwrap = $('<span class="tttinputwrap">').appendTo(_this.$el);
-        _this.$input = $('<span class="tttinput"/>').appendTo(_this.$inputwrap);
-        _this.$input.on('focus', _this._onInputFocus);
-        _this.$input.on('blur', _this._onInputBlur);
-        _this.$input.on('keydown', _this._onInputKeyDown);
-        _this.$input.on('keyup', _this._onInputKeyUp);
+    };
+
+    TTTags.prototype._buildListAndInput = function() {
+      var $ol, s,
+        _this = this;
+      if (!this.$input) {
+        this.$inputwrap = $('<span class="tttinputwrap">').appendTo(this.$el);
+        this.$input = $('<span class="tttinput"/>').appendTo(this.$inputwrap);
+        this.$input.on('focus', this._onInputFocus);
+        this.$input.on('blur', this._onInputBlur);
+        this.$input.on('keydown', this._onInputKeyDown);
+        this.$input.on('keyup', this._onInputKeyUp);
       }
-      if (_this.$list) {
+      if (this.$list) {
         return;
       }
-      _this.$list = $('<div class="tttaglist">').appendTo(_this.$inputwrap);
-      $ol = $('<ol>').appendTo(_this.$list);
-      s = typeof _this.src === 'function' ? _this.src() : [];
+      this.$list = $('<div class="tttaglist">').appendTo(this.$inputwrap);
+      $ol = $('<ol>').appendTo(this.$list);
+      s = typeof this.src === 'function' ? this.src() : [];
       if (!$.isArray(s)) {
         s = [];
       }
       return s.forEach(function(tag) {
         return $('<li>').text(tag).appendTo($ol);
       });
-    },
-    _suggest: function(txt) {
-      var $sel, ltxt, show;
-      if (_this.lastSuggest !== txt) {
+    };
+
+    TTTags.prototype._suggest = function(txt) {
+      var $sel, ltxt, show,
+        _this = this;
+      if (this.lastSuggest !== txt) {
         ltxt = txt.toLowerCase();
-        $('li', _this.$list).each(function(_, el) {
+        $('li', this.$list).each(function(_, el) {
           var $el;
           $el = $(el);
           return $el.toggleClass(ON, $el.text().toLowerCase().indexOf(ltxt) === 0);
         });
-        _this.lastSuggest = txt;
-        $sel = $('li.' + SEL, _this.$list);
+        this.lastSuggest = txt;
+        $sel = $('li.' + SEL, this.$list);
         if (!$sel.hasClass(ON)) {
           $sel.removeClass(SEL);
         }
-        show = txt.length > 0 && $('li.' + ON, _this.$list).length > 0;
-        return _this.$list.toggleClass(ON, show);
+        show = txt.length > 0 && $('li.' + ON, this.$list).length > 0;
+        return this.$list.toggleClass(ON, show);
       }
-    },
-    _listStep: function(ev, up) {
+    };
+
+    TTTags.prototype._listStep = function(ev, up) {
       var $sel, lh, ls, sh, st, tmp;
-      if (!_this.$list.hasClass(ON)) {
+      if (!this.$list.hasClass(ON)) {
         return;
       }
-      $sel = $('li.' + SEL, _this.$list);
+      $sel = $('li.' + SEL, this.$list);
       if ($sel.length) {
         if (up && (tmp = $sel.prevAll('li.' + ON)).length) {
           $sel.removeClass(SEL);
@@ -278,114 +309,126 @@
         }
       } else {
         if (up) {
-          $('li.' + ON, _this.$list).last().addClass(SEL);
+          $('li.' + ON, this.$list).last().addClass(SEL);
           ev.preventDefault();
         } else {
-          $('li.' + ON, _this.$list).first().addClass(SEL);
+          $('li.' + ON, this.$list).first().addClass(SEL);
           ev.preventDefault();
         }
       }
-      $sel = $('li.' + SEL, _this.$list);
+      $sel = $('li.' + SEL, this.$list);
       if ($sel.length === 0) {
         return;
       }
-      lh = _this.$list.outerHeight();
-      ls = _this.$list.scrollTop();
+      lh = this.$list.outerHeight();
+      ls = this.$list.scrollTop();
       st = $sel[0].offsetTop;
       sh = $sel.outerHeight();
       if ((st + sh) > (lh + ls)) {
-        _this.$list.scrollTop((st + sh) - lh);
+        this.$list.scrollTop((st + sh) - lh);
       }
       if (st < ls) {
-        return _this.$list.scrollTop(st);
+        return this.$list.scrollTop(st);
       }
-    },
-    _doSelect: function() {
+    };
+
+    TTTags.prototype._doSelect = function() {
       var $sel;
-      $sel = $('li.' + SEL, _this.$list);
+      $sel = $('li.' + SEL, this.$list);
       if ($sel.length) {
-        _this.add($sel.text(), {
+        this.add($sel.text(), {
           force: true
         });
       } else {
-        _this.add(_this.$input.text(), {
+        this.add(this.$input.text(), {
           force: false
         });
       }
-      _this.$input.text('');
-      return _this._suggest('');
-    },
-    _maybeShowPlaceholder: function() {
-      return $('.tttplaceholder', _this.$el).toggleClass(ON, $('.tag', _this.$el).length === 0);
-    },
-    _onMouseDown: function(ev) {
+      this.$input.text('');
+      return this._suggest('');
+    };
+
+    TTTags.prototype._maybeShowPlaceholder = function() {
+      return $('.tttplaceholder', this.$el).toggleClass(ON, $('.tag', this.$el).length === 0);
+    };
+
+    TTTags.prototype._onMouseDown = function(ev) {
       var $t, li;
       $t = $(ev.target);
       if ((li = $t.closest('li')).length > 0) {
-        $('li.' + SEL, _this.$list).removeClass(SEL);
+        $('li.' + SEL, this.$list).removeClass(SEL);
         li.addClass(SEL);
-        _this._doSelect();
+        this._doSelect();
       }
-      if (_this.isActive()) {
+      if (this.isActive()) {
         return ev.preventDefault();
       }
-    },
-    _onClick: function(ev) {
+    };
+
+    TTTags.prototype._onClick = function(ev) {
       var $t;
       $t = $(ev.target);
-      return _this.focus();
-    },
-    _onFocus: function(ev) {
-      return _this.focus();
-    },
-    _onInputFocus: function(ev) {
-      if (_this.isActive()) {
+      return this.focus();
+    };
+
+    TTTags.prototype._onFocus = function(ev) {
+      return this.focus();
+    };
+
+    TTTags.prototype._onInputFocus = function(ev) {
+      if (this.isActive()) {
         return;
       }
-      _this.$el.addClass(ACT);
-      return _this._trigger('focus', 'onFocus');
-    },
-    _onInputBlur: function(ev) {
-      if (!_this.isActive()) {
+      this.$el.addClass(ACT);
+      return this._trigger('focus', 'onFocus');
+    };
+
+    TTTags.prototype._onInputBlur = function(ev) {
+      if (!this.isActive()) {
         return;
       }
-      _this.$el.removeClass(ACT);
-      _this.$input.text('').removeAttr('contenteditable');
-      _this._suggest('');
-      $('li.' + SEL, _this.$list).removeClass(SEL);
-      _this._maybeShowPlaceholder();
-      return _this._trigger('blur', 'onBlur');
-    },
-    _onInputKeyDown: function(ev) {
+      this.$el.removeClass(ACT);
+      this.$input.text('').removeAttr('contenteditable');
+      this._suggest('');
+      $('li.' + SEL, this.$list).removeClass(SEL);
+      this._maybeShowPlaceholder();
+      return this._trigger('blur', 'onBlur');
+    };
+
+    TTTags.prototype._onInputKeyDown = function(ev) {
       var range, _ref;
       switch (ev.keyCode) {
         case 13:
           ev.preventDefault();
-          return _this._doSelect();
+          return this._doSelect();
         case 27:
-          return _this.$input.blur();
+          return this.$input.blur();
         case 8:
           range = typeof window !== "undefined" && window !== null ? window.getSelection().getRangeAt(0) : void 0;
           if (range) {
             if ((range.startOffset === (_ref = range.endOffset) && _ref === 0)) {
-              return _this.removeLast();
+              return this.removeLast();
             }
           } else {
-            if (_this.$input.text() === '') {
-              return _this.removeLast();
+            if (this.$input.text() === '') {
+              return this.removeLast();
             }
           }
           break;
         case 38:
-          return _this._listStep(ev, true);
+          return this._listStep(ev, true);
         case 40:
-          return _this._listStep(ev, false);
+          return this._listStep(ev, false);
       }
-    },
-    _onInputKeyUp: function(ev) {
-      return _this._suggest(_this.$input.text());
-    }
-  });
+    };
+
+    TTTags.prototype._onInputKeyUp = function(ev) {
+      return this._suggest(this.$input.text());
+    };
+
+    return TTTags;
+
+  })();
 
   $.fn.tttags = function(action, data) {
     var obj, opts;

commit bcb51de2ecc94ca7fdba0e2b08008a5422bcc7a5

removed incorrect <span> wrapping introduced in 0f36196

src/tttags.coffee
@@ -12,7 +12,7 @@ subst = (expr, map) ->
 
 defaultOpts =
     placeholder : ''
-    template    : '<a class="tag">#{tag}</a> '
+    template    : '<a class="tag">#{tag}</a>'
     className   : 'label label-default'
     allowNew    : true
     source      : ->
@@ -81,12 +81,14 @@ class TTTags
         return this unless @opts.allowNew or opts.force or (inList = @inList tag)
         $el = $ subst @opts.template, tag:tag
         $el.addClass @opts.className if @opts.className
-        $el = $('<span>').append($el).append(' ')
         if @$inputwrap
             @$inputwrap.before($el)
         else
-            @$el.prepend $el if $('.tag', @$el).parent().last().after($el).length == 0
-
+            # try insert after last
+            if $('.tag', @$el).last().after($el).length == 0
+                # fall back on inserting first
+                @$el.prepend $el
+        $el.after(' ')
         @_trigger 'add', 'onTagAdd', tag, $el, !inList unless opts.silent
         this
 

src/tttags.less
@@ -1,7 +1,7 @@
 @import "bower_components/bootswatchtt/tt/variables.less";
 
 @liheight: 1.8em;
-@tagspacing: 0em;
+@tagspacing: 0;
 @inputcolor: @gray;
 
 .tttags {
@@ -17,7 +17,7 @@
     }
     .tttinputwrap {
         position: relative;
-		margin-left: @tagspacing;
+        margin-left: @tagspacing;
         .tttinput {
             color: @inputcolor;
             display: inline-block;

tttags.css
@@ -12,7 +12,7 @@
 }
 .tttags .tttinputwrap {
   position: relative;
-  margin-left: 0em;
+  margin-left: 0;
 }
 .tttags .tttinputwrap .tttinput {
   color: #555555;
@@ -76,5 +76,5 @@
   content: '\00a0\00a0';
 }
 .tttags .tag {
-  margin-left: 0em;
+  margin-left: 0;
 }

tttags.js
@@ -1,4 +1,4 @@
-// Generated by CoffeeScript 1.6.3
+// Generated by CoffeeScript 1.7.1
 (function() {
   var $, ACT, ON, SEL, TTTags, defaultOpts, subst,
     __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
@@ -25,7 +25,7 @@
 
   defaultOpts = {
     placeholder: '',
-    template: '<a class="tag">#{tag}</a> ',
+    template: '<a class="tag">#{tag}</a>',
     className: 'label label-default',
     allowNew: true,
     source: function() {},
@@ -75,8 +75,7 @@
     }
 
     TTTags.prototype.source = function(source) {
-      var tags, _ref,
-        _this = this;
+      var tags, _ref;
       if ((_ref = this.$list) != null) {
         _ref.remove();
       }
@@ -87,11 +86,13 @@
         this._suggest(this.$input.text());
       }
       if ((tags = this.tags()).length) {
-        tags.forEach(function(tag) {
-          if (!_this.inList(tag)) {
-            return _this.remove(tag);
-          }
-        });
+        tags.forEach((function(_this) {
+          return function(tag) {
+            if (!_this.inList(tag)) {
+              return _this.remove(tag);
+            }
+          };
+        })(this));
         this._maybeShowPlaceholder();
       }
       return this;
@@ -155,14 +156,14 @@
       if (this.opts.className) {
         $el.addClass(this.opts.className);
       }
-      $el = $('<span>').append($el).append(' ');
       if (this.$inputwrap) {
         this.$inputwrap.before($el);
       } else {
-        if ($('.tag', this.$el).parent().last().after($el).length === 0) {
+        if ($('.tag', this.$el).last().after($el).length === 0) {
           this.$el.prepend($el);
         }
       }
+      $el.after(' ');
       if (!opts.silent) {
         this._trigger('add', 'onTagAdd', tag, $el, !inList);
       }
@@ -170,22 +171,25 @@
     };
 
     TTTags.prototype.tags = function(toSet) {
-      var _this = this;
       if (typeof toSet === 'string') {
         toSet = [toSet];
       }
       if ($.isArray(toSet)) {
-        this.tags().forEach(function(tag) {
-          return _this.remove(tag, {
-            silent: true
-          });
-        });
-        toSet.forEach(function(tag) {
-          return _this.add(tag, {
-            force: true,
-            silent: true
-          });
-        });
+        this.tags().forEach((function(_this) {
+          return function(tag) {
+            return _this.remove(tag, {
+              silent: true
+            });
+          };
+        })(this));
+        toSet.forEach((function(_this) {
+          return function(tag) {
+            return _this.add(tag, {
+              force: true,
+              silent: true
+            });
+          };
+        })(this));
         this._maybeShowPlaceholder();
         return this;
       } else {
@@ -247,8 +251,7 @@
     };
 
     TTTags.prototype._buildListAndInput = function() {
-      var $ol, s,
-        _this = this;
+      var $ol, s;
       if (!this.$input) {
         this.$inputwrap = $('<span class="tttinputwrap">').appendTo(this.$el);
         this.$input = $('<span class="tttinput"/>').appendTo(this.$inputwrap);
@@ -266,21 +269,24 @@
       if (!$.isArray(s)) {
         s = [];
       }
-      return s.forEach(function(tag) {
-        return $('<li>').text(tag).appendTo($ol);
-      });
+      return s.forEach((function(_this) {
+        return function(tag) {
+          return $('<li>').text(tag).appendTo($ol);
+        };
+      })(this));
     };
 
     TTTags.prototype._suggest = function(txt) {
-      var $sel, ltxt, show,
-        _this = this;
+      var $sel, ltxt, show;
       if (this.lastSuggest !== txt) {
         ltxt = txt.toLowerCase();
-        $('li', this.$list).each(function(_, el) {
-          var $el;
-          $el = $(el);
-          return $el.toggleClass(ON, $el.text().toLowerCase().indexOf(ltxt) === 0);
-        });
+        $('li', this.$list).each((function(_this) {
+          return function(_, el) {
+            var $el;
+            $el = $(el);
+            return $el.toggleClass(ON, $el.text().toLowerCase().indexOf(ltxt) === 0);
+          };
+        })(this));
         this.lastSuggest = txt;
         $sel = $('li.' + SEL, this.$list);
         if (!$sel.hasClass(ON)) {